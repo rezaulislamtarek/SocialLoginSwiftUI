0407c2e748e7f5270c70c68eb3e480729b25f444
rezaulislamtarek <mrezaulislamtarek.t@gmail.com>
2024-06-08 16:16:03 +0600

    Initial Commit

---
+++ SocialLoginSwiftUI/SocialLoginSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialLoginSwiftUIApp.swift
+//  SocialLoginSwiftUI
+//
+//  Created by Rezaul Islam on 8/6/24.
+//
+
+import SwiftUI
+
+@main
+struct SocialLoginSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
