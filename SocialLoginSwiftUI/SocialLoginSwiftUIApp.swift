f1fa9fce4971b6c4e0be2321d4cd31e245ff9458
rezaulislamtarek <mrezaulislamtarek.t@gmail.com>
2024-06-24 11:19:17 +0600

    Update

--- SocialLoginSwiftUI/SocialLoginSwiftUIApp.swift
+++ SocialLoginSwiftUI/SocialLoginSwiftUIApp.swift
@@ -6,12 +6,23 @@
 //
 
 import SwiftUI
+ 
 
 @main
 struct SocialLoginSwiftUIApp: App {
+     
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
 }
+
+
+ 
+
+
+
+
+
+
